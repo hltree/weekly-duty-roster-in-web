6c9e169d22058782cf8b2602c15ee726c7817e78
hltree <35206336+hltree@users.noreply.github.com>
2020-10-26 23:25:11 +0900

    chore: test home

---
+++ data/laravel/resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.base')
+@section('content')
+    <p>Welcome to home!</p>
+@endsection
---
+++ data/laravel/resources/views/layout/base.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <title>Web Duty Roster with Web</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
